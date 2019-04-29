4b0e3785a3bfef481d3eb0abb47565bd990c4e90
camcam1132000 <camcam1132000@gmail.com>
2019-04-29 19:30:34 +0700

    Thay doi: them model, migration, controller

---
+++ app/Http/Controllers/API/SchoolsActivityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\SchoolsActivity;
+use Illuminate\Http\Request;
+
+class SchoolsActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SchoolsActivity  $schoolsActivity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SchoolsActivity $schoolsActivity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SchoolsActivity  $schoolsActivity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SchoolsActivity $schoolsActivity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SchoolsActivity  $schoolsActivity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SchoolsActivity $schoolsActivity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SchoolsActivity  $schoolsActivity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SchoolsActivity $schoolsActivity)
+    {
+        //
+    }
+}
